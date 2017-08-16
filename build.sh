395aced93259a739d4dd9a5f8d1607795c0b5933
nazunamoe <jpg3927@gmail.com>
2017-08-16 16:33:53 +0900

    error and asuka

--- build.sh
+++ build.sh
@@ -17,11 +17,6 @@ export ARCH=arm64
 export CROSS_COMPILE="/home/guyver/Desktop/Toolchain/Uber-v6.4.1/bin/aarch64-linux-android-"
 export LD_LIBRARY_PATH=home/guyver/Desktop/Toolchain/Uber-v6.4.1/lib/
 STRIP="/home/guyver/Desktop/Toolchain/Uber-v6.4.1/bin/aarch64-linux-android-strip"
-make clean
-make guyver_defconfig
-export KBUILD_BUILD_HOST="Ubuntu"
-export KBUILD_BUILD_USER="GUYVER"
-make -j5
 time=$(date +"%d-%m-%y-%T")
 $DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
 mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/dtb
